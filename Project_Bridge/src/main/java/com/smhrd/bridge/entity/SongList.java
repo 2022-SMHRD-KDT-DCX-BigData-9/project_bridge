ce700077cf81824ea5ea769a806e2e6bfd3af5f6
smhrd <smhrd@DESKTOP-JI8Q0GP>
2023-12-11 14:04:17 +0900

    23.12.11 (14:11) JPA 객체 생성 (조정흠)

--- Project_Bridge/src/main/java/com/smhrd/bridge/entity/SongList.java
+++ Project_Bridge/src/main/java/com/smhrd/bridge/entity/SongList.java
@@ -1,10 +1,21 @@
 package com.smhrd.bridge.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
 
 @Entity
 @Table(name = "songlist")
 public class SongList {
-
+	@Id
+	@Column(name = "song_num")
+	private int song_num;
+	
+	@Column(name = "song_name", length = 100, nullable = false)
+	private String song_name;
+	
+	@Column(name = "singer", length = 30, nullable = false)
+	private String singer;
 }
--- Project_Bridge/src/main/java/com/smhrd/bridge/entity/UserInfo.java
+++ Project_Bridge/src/main/java/com/smhrd/bridge/entity/UserInfo.java
@@ -1,10 +1,37 @@
 package com.smhrd.bridge.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Table(name = "userinfo")
 public class UserInfo {
+	@Id
+	@Column(name = "user_id", length = 20, nullable = false)
+	private String user_id;
+	
+	@Column(name = "user_pw", length = 50, nullable = false)
+	private String user_pw;
 
+	@Column(name = "user_name", length = 20, nullable = false)
+	private String user_name;
+
+	@Column(name = "user_nick", length = 20, nullable = false)
+	private String user_nick;
+	
+	@Column(name = "user_birthday", nullable = false)
+	private Date user_birthday;
+	
+	@CreationTimestamp // 실제 시간 읽기
+	@Column(name = "join_birthday", nullable = false)
+	private Date join_birthday;
+
+	@Column(name = "user_address", nullable = false)
+	private String user_address;
 }
